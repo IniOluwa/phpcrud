d99a1e8af8f7eefb0c98d3bc897dcb28e73f9ab9
IniOluwa <inioluwafageyinbo@gmail.com>
2016-11-19 18:38:42 +0100

    logout.php referenced in edit.php

--- book/edit.php
+++ book/edit.php
@@ -75,7 +75,7 @@
                         </ul>
                         <ul class="nav navbar-nav navbar-right">
                             <li><a href="#"><span class="glyphicon glyphicon-user"></span><?php echo " " . $_SESSION['username']?></a></li>
-                            <li><a href="logout"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>
+                            <li><a href="logout.php"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>
                         </ul>
                     </div>
                 </div>
